d777742719d7b13e9006ff9698bb39e8b0960dbb
Jeff Quinn <ns8riders@me.com>
2017-05-31 20:42:47 -0600

    page 28

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,33 @@ class ProductList extends React.Component {
   render() {
     return (
       <div className='ui unstackable items'>
-        Hello, Friend! I am a basic React Component.
+        <Product />
+      </div>
+    );
+  }
+}
+
+class Product extends React.Component {
+  render() {
+    return(
+      <div className="item">
+        <div className="image">
+          <img src='images/products/image-aqua.png' />
+        </div>
+        <div className="middle aligned content">
+          <div className="description">
+            <a>Fort Knight</a>
+            <p>Authentic renaissance actors, delivered in two thousand years.</p>
+          </div>
+          <div className="extra">
+            <span>submitted by:</span>
+            <img
+              className='ui avatar image'
+              src="images/avatars/daniel.jpg"
+              alt="img of daniel"
+            />
+          </div>
+        </div>
       </div>
     );
   }
